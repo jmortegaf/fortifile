bba1b0c631728c0fbaeebaff4a764c43bd5a46de
Jose Ortega <jmortegafreire@gmail.com>
2025-01-01 15:40:01 -0300

    Starting project

---
+++ src/main/java/com/jmortegaf/fortifile/FortifileApplication.java
@@ -0,0 +1,13 @@
+package com.jmortegaf.fortifile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortifileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortifileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jmortegaf/fortifile/FortifileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmortegaf.fortifile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortifileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
